a6ec9e97197b40a4616b1f8d6bed2b5e287650c1
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-03-01 00:24:25 +0200

    Seeding Database

--- ABPBackendTZ/ApplicationDbContext.cs
+++ ABPBackendTZ/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using ABPBackendTZ.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ABPBackendTZ;
@@ -6,6 +7,48 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-            
+    }
+
+    public DbSet<ButtonColor> ButtonColors { get; set; }
+    public DbSet<PriceChange> PriceChanges { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<ButtonColor>().HasData(new ButtonColor
+        {
+            Id = 1,
+            HEX = "#FF0000",
+        }, new ButtonColor
+        {
+            Id = 2,
+            HEX = "#00FF00",
+        }, new ButtonColor
+        {
+            Id = 3,
+            HEX = "#0000FF",
+        });
+
+        builder.Entity<PriceChange>().HasData(new
+        {
+            Id = 1,
+            Value = 10M,
+            Percentage = 0.75F,
+        }, new
+        {
+            Id = 2,
+            Value = 20M,
+            Percentage = 0.1F,
+        }, new
+        {
+            Id = 3,
+            Value = 50M,
+            Percentage = 0.05F,
+        }, new
+        {
+            Id = 4,
+            Value = 5M,
+            Percentage = 0.1F,
+        });
     }
 }
---
+++ ABPBackendTZ/Migrations/20240229222231_SeedingDb.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ABPBackendTZ.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ButtonColors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HEX = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ButtonColors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceChanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Percentage = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceChanges", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ButtonColors",
+                columns: new[] { "Id", "HEX" },
+                values: new object[,]
+                {
+                    { 1, "#FF0000" },
+                    { 2, "#00FF00" },
+                    { 3, "#0000FF" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PriceChanges",
+                columns: new[] { "Id", "Percentage", "Value" },
+                values: new object[,]
+                {
+                    { 1, 0.75f, 10m },
+                    { 2, 0.1f, 20m },
+                    { 3, 0.05f, 50m },
+                    { 4, 0.1f, 5m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ButtonColors");
+
+            migrationBuilder.DropTable(
+                name: "PriceChanges");
+        }
+    }
+}
