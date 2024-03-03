78863468e140746bee55301b21b374a0b2eff2e6
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-03-03 08:39:43 +0200

    Refactoring statistics program

--- ABPBackendGetStatistics/Program.cs
+++ ABPBackendGetStatistics/Program.cs
@@ -3,41 +3,57 @@ using Newtonsoft.Json.Linq;
 
 class Program
 {
+    class Argument
+    {
+        private int quantity;
+        public int Quantity { get; init; }
+        public string Template { get; init; }
+        public string Url { get; init; }
+        public Dictionary<string, int> valuesCounter;
+    }
     static async Task Main(string[] args)
     {
-        string buttonColorTemplate = "button-color";
-        string priceTemplate = "price";
-        string deviceButtonEndPoint = "https://localhost:7080/experiment/button-color?device_token=";
-        string priceEndPoint = "https://localhost:7080/experiment/price?device_token=";
-        
-        Dictionary<string, int> valuesCounterForButtons = new Dictionary<string, int>
+        Argument buttonColorArgument = new()
         {
-            { "#FF0000", 0 },
-            { "#00FF00", 0 },
-            { "#0000FF", 0 },
-            { "Total", 0 }
-        };
-        Dictionary<string, int> valuesCounterForPrices = new Dictionary<string, int>
-        {
-            { "50,00", 0 },
-            { "20,00", 0 },
-            { "10,00", 0 },
-            { "5,00", 0 },
-            { "Total", 0 }
+            Quantity = 2000,
+            Template = "button-color",
+            Url = "https://localhost:7080/experiment/button-color?device_token=",
+            valuesCounter = new()
+            {
+                { "#FF0000", 0 },
+                { "#00FF00", 0 },
+                { "#0000FF", 0 },
+                { "Total", 0 }
+            }
         };
 
-        await CalculateStatistics(1000, priceTemplate, priceEndPoint, valuesCounterForPrices);
+        Argument priceArgument = new()
+        {
+            Quantity = 2000,
+            Template = "price",
+            Url = "https://localhost:7080/experiment/price?device_token=",
+            valuesCounter = new()
+            {
+                { "50,00", 0 },
+                { "20,00", 0 },
+                { "10,00", 0 },
+                { "5,00", 0 },
+                { "Total", 0 }
+            }
+        };
+
+        await CalculateStatistics(priceArgument);
     }
 
-    static async Task CalculateStatistics(int devices, string template, string url, Dictionary<string, int> valuesCounter)
+    static async Task CalculateStatistics(Argument argument)
     {
-        string filePath = $"/Users/bondarenkooleksandr/C#Projects/ABPBackendTZ/ABPBackendGetStatistics/{template}-results-for-{devices}.json";
+        string filePath = $"/Users/bondarenkooleksandr/C#Projects/ABPBackendTZ/ABPBackendGetStatistics/{argument.Template}-results-for-{argument.Quantity}.json";
         HttpClient client = new HttpClient();
         List<string> results = new List<string>();
 
-        for (int i = 0; i < devices; i++)
+        for (int i = 0; i < argument.Quantity; i++)
         {
-            HttpResponseMessage response = await client.GetAsync(url + i);
+            HttpResponseMessage response = await client.GetAsync(argument.Url + i);
             if (response.IsSuccessStatusCode)
             {
                 string result = await response.Content.ReadAsStringAsync();
@@ -50,16 +66,16 @@ class Program
             var deserializedResult = JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
             string value = deserializedResult["value"];
 
-            if (valuesCounter.ContainsKey(value))
+            if (argument.valuesCounter.ContainsKey(value))
             {
-                valuesCounter[value]++;
-                valuesCounter["Total"]++;
+                argument.valuesCounter[value]++;
+                argument.valuesCounter["Total"]++;
             }
         }
 
         JObject finalJson = new JObject();
         finalJson["results"] = JArray.FromObject(results);
-        finalJson["values_counter"] = JObject.FromObject(valuesCounter);
+        finalJson["values_counter"] = JObject.FromObject(argument.valuesCounter);
         await File.WriteAllTextAsync(filePath, finalJson.ToString(Formatting.Indented));
 
         Console.WriteLine($"Results are saved in {filePath}");
